ddb9dd87fc54545a29c83718ec34d99d079bb1b4
smaacg <smaacg@gmail.com>
2026-07-27 21:38:01 +0800

    Update functions.php

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -80,7 +80,6 @@ $optional = [
     'image-optimizer',
     'ajax-news-filter',
     'search-hot',        // ★v2.25.0 熱門/最近搜尋 REST（有 file_exists 保護）
-    'wpdiscuz-spoiler-button',   // ★ 加這行
 ];
 
 
